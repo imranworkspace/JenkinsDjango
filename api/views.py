63a3588d91dcf2c894304b73107c3e75024a6832
imranlatur24 <imranlatur24@gmail.com>
2025-10-01 19:14:00 +0530

    update Jenkins

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h3>hello imran shaikh</h3>")
+    return HttpResponse("<h3>hello imran shaikh....................</h3>")
